2b041fe865d56ccbbe1da93ad6f41dfd2734466b
Jan Rembold <janrembold@googlemail.com>
2016-02-19 21:32:34 +0100

    initial commit

--- js/modr.jquery.boilerplate.module.js
+++ js/modr.jquery.boilerplate.module.js
@@ -1,16 +1,3 @@
-# modr.jquery.boilerplate
-
-Any module should follow some structural and coding conventions:
- 
-- Use boilerplate code shown below to kickstart your module
-- Set modules default options in configs `defaults` object
-- Set dependencies to other modr modules in configs `dependencies` object
-- Add optional `prepare()` function if anything should be executed before module gets initialized, e.g. listeners that init the module
-- Use `init()` and `destroy()` functions that create/destroy plugins functionality
-- Remove all variables, listeners and additional classes set to plugins element. Reset anything to the default state
-- Use `var self = this;` in any function to access modules public variables/methods
-
-```js
 (function($) {
     'use strict';
 
@@ -46,4 +33,3 @@ Any module should follow some structural and coding conventions:
     });
 
 })(jQuery);
-```
